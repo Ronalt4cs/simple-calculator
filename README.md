8521679ac7271c1828266ceb60dfa66536bcfdb9
ronalt augusto <ronaltaugustocs@gmmail.com>
2023-01-21 09:44:46 -0300

    added change theme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Simple calculator
+
+>Fronten Mentor challenge
+
+Basic calculator made only with HTML, CSS and Javascript
+
+
+## Functions
+
+- Sum
+- Subtract
+- Divide
+- Multiply
+- Change theme
+
+## Preview
+
+![preview](images/desktop-preview.jpg)
+
+## 🔗 Link to test
+
+`press CTRL + click to open in new tab`
+
+https://ronalt4cs.github.io/simple-calculator/
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const screenResult = document.querySelector('.screen-result');
 const body = document.querySelector('body');
+const root = document.querySelector(':root');
+const screenResult = document.querySelector('.screen-result');
 const numbers = document.querySelectorAll('.number');
 const btnSum = document.querySelector('.btn-sum');
 const btnMinus = document.querySelector('.btn-minus');
@@ -9,6 +10,11 @@ const btnEqual = document.querySelector('.btn-equal');
 const btnDot = document.querySelector('.btn-dot');
 const btnDelete = document.querySelector('.btn-del');
 const btnReset = document.querySelector('.btn-reset');
+
+const btnThemeOne = document.getElementById('one');
+const btnThemeTwo = document.getElementById('two');
+const btnThemeThree = document.getElementById('three');
+const circleTheme = document.getElementById('circle');
 
 const maxDigitsOnScreen = 12;
 let operation = '';
@@ -21,6 +27,78 @@ const clearAllOperations = () => {
    operation = '';
    percente = '';
 }
+
+const changeTheme = (newTheme) => {
+   if (newTheme === 1) {
+      root.style.setProperty('--bg-color', 'hsl(222, 26%, 31%)');
+      root.style.setProperty('--bg-keyboard', 'hsl(223, 31%, 20%)');
+      root.style.setProperty('--bg-screen', 'hsl(224, 36%, 15%)');
+      root.style.setProperty('--bg-key', 'hsl(30, 25%, 89%)');
+
+      root.style.setProperty('--btn-secondary', 'hsl(225, 21%, 49%)');
+      root.style.setProperty('--btn-spotlight', 'hsl(6, 63%, 50%)');
+      root.style.setProperty('--key-shadow', 'hsl(28, 16%, 65%)');
+      root.style.setProperty('--secondary-key-shadow', 'hsl(224, 28%, 35%)');
+      root.style.setProperty('--spotlight-key-shadow', 'hsl(6, 70%, 34%)');
+
+      root.style.setProperty('--txt-color', 'hsl(221, 14%, 31%)');
+      root.style.setProperty('--txt-secondary', 'hsl(0, 0%, 100%)');
+      btnEqual.style.color = 'hsl(0, 0%, 100%)';
+      screenResult.style.color = 'hsl(0, 0%, 100%)';
+      circleTheme.style.left = 0;
+      return
+   }
+   if (newTheme === 2) {
+      root.style.setProperty('--bg-color', 'hsl(0, 0%, 90%)');
+      root.style.setProperty('--bg-keyboard', 'hsl(0, 5%, 81%)');
+      root.style.setProperty('--bg-screen', 'hsl(0, 0%, 93%)');
+      root.style.setProperty('--bg-key', 'hsl(45, 7%, 89%)');
+
+      root.style.setProperty('--btn-secondary', 'hsl(185, 42%, 37%)');
+      root.style.setProperty('--btn-spotlight', 'hsl(25, 98%, 40%)');
+      root.style.setProperty('--key-shadow', 'hsl(35, 11%, 61%)');
+      root.style.setProperty('--secondary-key-shadow', 'hsl(185, 58%, 25%)');
+      root.style.setProperty('--spotlight-key-shadow', 'hsl(25, 99%, 27%)');
+
+      root.style.setProperty('--txt-color', 'hsl(60, 10%, 19%)');
+      root.style.setProperty('--txt-secondary', 'hsl(60, 10%, 19%)');
+      btnEqual.style.color = 'hsl(0, 0%, 100%)';
+      screenResult.style.color = 'hsl(60, 10%, 19%)';
+      circleTheme.style.left = '22px';
+      return
+   }
+   if (newTheme === 3) {
+      root.style.setProperty('--bg-color', 'hsl(268, 75%, 9%)');
+      root.style.setProperty('--bg-keyboard', 'hsl(268, 71%, 12%)');
+      root.style.setProperty('--bg-screen', 'hsl(268, 71%, 12%)');
+      root.style.setProperty('--bg-key', 'hsl(281, 89%, 26%)');
+
+      root.style.setProperty('--btn-secondary', 'hsl(268, 47%, 21%)');
+      root.style.setProperty('--btn-spotlight', 'hsl(176, 100%, 44%)');
+      root.style.setProperty('--key-shadow', 'hsl(290, 70%, 36%)');
+      root.style.setProperty('--secondary-key-shadow', 'hsl(285, 91%, 52%)');
+      root.style.setProperty('--spotlight-key-shadow', 'hsl(177, 92%, 70%)');
+
+      root.style.setProperty('--txt-color', 'hsl(52, 100%, 62%)');
+      root.style.setProperty('--txt-secondary', 'hsl(52, 100%, 62%)');
+      btnEqual.style.color = 'hsl(198, 20%, 13%)';
+      screenResult.style.color = 'hsl(52, 100%, 62%)';
+
+      circleTheme.style.left = '44px';
+      return
+   }
+}
+
+btnThemeOne.addEventListener('click', () => {
+   changeTheme(1);
+})
+
+btnThemeTwo.addEventListener('click', () => {
+   changeTheme(2);
+})
+btnThemeThree.addEventListener('click', () => {
+   changeTheme(3)
+})
 
 btnReset.addEventListener('click', () => {
    screenResult.textContent = '';
